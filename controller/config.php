a9ea23f1affe045411143801760c87ab712ce70c
martti <info@martti.be>
2017-09-21 14:05:09 +0200

    config pages frame tpl

--- controller/config.php
+++ controller/config.php
@@ -11,5 +11,74 @@ class config
 	{
 		return $app['twig']->render('config/a_index.html.twig', []);
 	}
-}
 
+	public function balance_limits(Request $request, app $app, string $schema)
+	{
+		return $app['twig']->render('config/a_balance_limits.html.twig', [
+
+		]);
+	}
+
+	public function ads(Request $request, app $app, string $schema)
+	{
+		return $app['twig']->render('config/a_ads.html.twig', [
+
+		]);
+	}
+
+	public function naming(Request $request, app $app, string $schema)
+	{
+		return $app['twig']->render('config/a_naming.html.twig', [
+
+		]);
+	}
+
+	public function mail_addresses(Request $request, app $app, string $schema)
+	{
+		return $app['twig']->render('config/a_mail_addresses.html.twig', [
+
+		]);
+	}
+
+	public function periodic_mail(Request $request, app $app, string $schema)
+	{
+		return $app['twig']->render('config/a_periodic_mail.html.twig', [
+
+		]);
+	}
+
+	public function contact_form(Request $request, app $app, string $schema)
+	{
+		return $app['twig']->render('config/a_contact_form.html.twig', [
+
+		]);
+	}
+
+	public function registration_form(Request $request, app $app, string $schema)
+	{
+		return $app['twig']->render('config/a_registration_form.html.twig', [
+
+		]);
+	}
+
+	public function forum(Request $request, app $app, string $schema)
+	{
+		return $app['twig']->render('config/a_forum.html.twig', [
+
+		]);
+	}
+
+	public function members(Request $request, app $app, string $schema)
+	{
+		return $app['twig']->render('config/a_members.html.twig', [
+
+		]);
+	}
+
+	public function system(Request $request, app $app, string $schema)
+	{
+		return $app['twig']->render('config/a_system.html.twig', [
+
+		]);
+	}
+}
--- routing.php
+++ routing.php
@@ -410,6 +410,26 @@ $config = $app['controllers_factory'];
 
 $config->match('/', 'controller\\config::index')
 	->bind('config_index');
+$config->match('/balance-limits', 'controller\\config::balance_limits')
+	->bind('config_balance_limits');
+$config->match('/ads', 'controller\\config::ads')
+	->bind('config_ads');
+$config->match('/naming', 'controller\\config::naming')
+	->bind('config_naming');
+$config->match('/mail-addresses', 'controller\\config::mail_addresses')
+	->bind('config_mail_addresses');
+$config->match('/periodic-mail', 'controller\\config::periodic_mail')
+	->bind('config_periodic_mail');
+$config->match('/contact-form', 'controller\\config::contact_form')
+	->bind('config_contact_form');
+$config->match('/registration-form', 'controller\\config::registration_form')
+	->bind('config_registration_form');
+$config->match('/forum', 'controller\\config::forum')
+	->bind('config_forum');
+$config->match('/members', 'controller\\config::members')
+	->bind('config_members');
+$config->match('/system', 'controller\\config::system')
+	->bind('config_system');
 
 $a->mount('/config', $config);
 
