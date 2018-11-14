babdc905b1ff890f02068bdd1ffc4f6fb1f315bf
martti <info@martti.be>
2018-11-14 17:00:42 +0100

    predis

---
+++ src/Controller/ExportController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExportController extends AbstractController
+{
+    /**
+     * @Route("/export", name="export")
+     */
+    public function index()
+    {
+        return $this->render('export/index.html.twig', [
+            'controller_name' => 'ExportController',
+        ]);
+    }
+}
