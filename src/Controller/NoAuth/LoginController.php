4422263d96b2ebc9dbd9784c487cb234270092e8
martti <info@martti.be>
2018-11-27 21:45:14 +0100

    login controller

--- src/Controller/NoAuth/LoginController.php
+++ src/Controller/NoAuth/LoginController.php
@@ -2,29 +2,28 @@
 
 namespace App\Controller\NoAuth;
 
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
-
 class LoginController extends AbstractController
 {
     /**
-     * Route("/login", name="login")
+     * @Route("/login", name="login")
      */
-    public function index(
-        Request $request,
+    public function login(
+        string $request,
         string $system,
-        SessionInterface $session):Response
+        AuthenticationUtils $authenticationUtils): Response
     {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-
-
-
-
-
-        return new Response($rsp);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 }
---
+++ src/Legacy/Render/ContactForm.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller\NoAuth;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ContactForm
+{
+    public function render(Request $request, string $system):string
+    {
+
+    }
+
+    public function post()
+    {
+
+    }
+}
